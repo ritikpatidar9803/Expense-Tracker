a867e13d5b079ea45877ed00044e6f5339347529
Ritik Patidar <92930815+ritikpatidar9803@users.noreply.github.com>
2026-06-15 19:09:55 +0530

    Delete frontend directory

--- frontend/src/utils/date.js
+++
@@ -1,9 +0,0 @@
-export const formatDate = (dateInput) =>{
-    const date = new Date(dateInput);
-
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = date.toLocaleString("en-US", {month:"short"})
-    const year = date.getFullYear();
-    
-    return `${day} ${month} ${year}`;
-}
