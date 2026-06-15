586d7a65f578e7a7f56cd90f5cbfaa23c25dcb31
Ritik Patidar <92930815+ritikpatidar9803@users.noreply.github.com>
2026-06-15 18:21:39 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,9 @@ dotenv.config()
 const app = express();
 
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT,()=>console.log(`Server is running on port `+ process.env.PORT));
+app.listen(PORT,()=>console.log(`Server is running on port `+ PORT));
 
 import mongoose from "mongoose";
 mongoose.connect(process.env.DB_URL, {
